c20d1cc5f2a12eec1aa874b90ca9c28c8322f584
Robert Fairley <rob.t.fairley@gmail.com>
2019-03-12 02:00:31 -0400

    nanopb basic example

---
+++ Development/F7_nanopbuf/F7_nanopbuf/nanopb-0.3.9.3-windows-x86/serial_echo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import serial
+import time
+
+import RobotCmd_pb2
+
+with serial.Serial('COM4', 230400, timeout=1) as ser:
+    for i in range(10):
+        message = RobotCmd_pb2.Hello()
+        message.message = b"hello"
+        orig = message.SerializeToString()
+        ser.write(orig)
+        data = ser.read(5)
+        print("iteration {}:\n  orig: {}\n  data: {}".format(i, orig, data))
+        
